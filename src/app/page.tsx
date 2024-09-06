ead74e6d798d8d7e087c1d7dc5692b0270f50d44
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-09-06 16:16:30 -0300

    Building Login component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Login from './components/Login';
 import { ThemeProvider } from './contexts/Themes/ThemeContext';
 import Home from './home/page';
 
 
 export default function App() {
   return (
-    <Home />
+    <Login />
+    // <Home />
   );
 }
